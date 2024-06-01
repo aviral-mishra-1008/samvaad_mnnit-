b3dc3238bf10584e6124ab0f881ac49a634d76b6
Aviral <aviralmishra10@gmail.com>
2024-06-01 16:02:21 +0530

    Preparing the repo for deployment on Azure

--- Samvad/wsgi.py
+++ Samvad/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
+setting_module = 'Samvad.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Samvad.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Samvad.settings')
 
