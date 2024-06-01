7ae26f43c008689d94a4ee9cc55a14fd9cfc407c
Aviral <aviralmishra10@gmail.com>
2024-06-01 17:50:14 +0530

    fixed yml file

--- Samvad/deployment.py
+++ Samvad/deployment.py
@@ -20,7 +20,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 l = connection_string.split(' ')
