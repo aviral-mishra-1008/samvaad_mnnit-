8a714ca22b9f17a2237e6ca6dba34aa32b72ebb6
Aviral <aviralmishra10@gmail.com>
2024-06-01 18:10:19 +0530

    fixed yml file

--- Samvad/settings.py
+++ Samvad/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 
