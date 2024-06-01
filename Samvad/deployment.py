8d20cac8bbb10809e16291d3e958da4e358cf452
Aviral <aviralmishra10@gmail.com>
2024-06-01 16:39:57 +0530

    Preparing to deploy project on Azure

--- Samvad/deployment.py
+++ Samvad/deployment.py
@@ -2,8 +2,10 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
-CSRF_TRUSTED_ORIGINS = ["https://"+ os.environ["WEBSITE_HOSTNAME"]]
+
+SECRET_KEY = os.environ['SECRET']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ["https://"+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
 MIDDLEWARE = [
